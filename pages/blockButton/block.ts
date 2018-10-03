6f1aa26c37ef48136e5a20ee180c12fa3bca4253
Sivanraj <38311880+Sivan95@users.noreply.github.com>
2018-10-03 11:31:04 +0800

    Add files via upload

---
+++ pages/blockButton/block.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: 'block.html'
+})
+export class blockButtonPage {}
---
+++ pages/size/size.ts
@@ -0,0 +1,6 @@
+import{Component} from '@angular/core';
+
+@Component({
+    templateUrl: "size.html"
+})
+export class SizePage {}
